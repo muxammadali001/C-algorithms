961e0b3387a488c0bccc85579d04d703b140feb1
Muxammad_Ali_21 <91970500+muxammadali001@users.noreply.github.com>
2022-05-28 23:48:49 +0500

    Optimize

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+namespace Algorithms
+{
+    static int findMiximum(int a, int b, int c)
+    {
+        if(a > b )
+        {
+            if (a > c || a == c)
+            {
+                return a;
+            }
+        }
+        if(b > a)
+        {
+            if (b > c || b == c)
+            {
+                return b;
+            }
+        }
+    }
+
+}
