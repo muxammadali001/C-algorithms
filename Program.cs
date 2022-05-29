d6fadbb3c5173da77db9efd642651fdaf479d2b2
Muxammad_Ali_21 <91970500+muxammadali001@users.noreply.github.com>
2022-05-29 22:46:25 +0500

    all

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿// using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-// class Program
-// {
+
+
+class Program
+ {
 //     static int findMaximum(int a, int b, int c)
 //     {
 //         if(a > b )
@@ -42,5 +46,40 @@
 //         Console.WriteLine(findMaximum2(1, 1, 9));
 //         Console.WriteLine(findMaximum2(1, 9, 9));
 //     }
+//     static Boolean IsUppercase(string s)
+//        {
+//               return s.All(char.IsUpper);
+//        }
+//        static Boolean IsLowercase(string s)
+//        {
+//               return s.All(char.IsLower);
+//        }
+//        static Boolean isPassword(string s)
+//        {
+//               return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
+//        }
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine(IsUppercase("hello!"));
+//            Console.WriteLine(IsUppercase("Hello "));
+//            Console.WriteLine(IsLowercase("Hello World!"));
+//            Console.WriteLine(IsLowercase("hello!"));
+//            Console.WriteLine(isPasswordComplex("HelloWorld1"));
+//        }
 
-// }
+        private static bool isPasswordComplex(string v)
+        {
+            throw new NotImplementedException();
+        }
+         static string NormalizeString(String input)
+        {
+           return input.ToLower().Trim().Replace(", ", "");
+        }
+    
+
+        static void Main1(string[] args)
+        {
+            Console.WriteLine(NormalizeString("Hello World!"));
+        }
+    }
+}
